6e00d076308a179bdc244a911b9d8dfb7eff3adf
Gerard <gkhynes@gmail.com>
2018-01-23 21:23:23 +0000

    Add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# Adding string times with reduce
+
+A script written to take string times from the DOM and add them. Part of Wes Bos' [JavaScript 30](https://javascript30.com/) course.
+
+## Notes
--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const seconds = timeNodes
   .map(node => node.dataset.time)
   .map(timeCode => {
     const [mins, secs] = timeCode.split(":").map(parseFloat);
+    // console.log(mins, secs);
     return mins * 60 + secs;
-    console.log(mins, secs);
   })
   .reduce((total, vidSeconds) => total + vidSeconds);
 
